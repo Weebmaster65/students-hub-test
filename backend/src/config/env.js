a3cebca71dd8e9212ad481883e181d764217a1b5
Rslc <raulleycar@hotmail.com>
2026-01-20 11:05:48 -0600

    [CREATED]: creamos estructura, agregamos env y config de env

---
+++ backend/src/config/env.js
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+
+export const env={
+PORT:Number(process.env.PORT || 500),
+CORS_ORIGIN: process.env.CORS_ORIGIN || '',
+FIREBASE_PROJECT_ID: process.FIREBASE_PROJECT_ID,
+FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+FIREBASE_PRIVATE_KEY: process.env.FIREVASE_PRIVATE_KEY,
+JWT_SECRET: process.env.JWT_SECRET,
+JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN
+}
+export function assertEnv(){
+const required =[
+	'FIREBASE_PROJECT_ID',
+	'FIREBASE_CLIENT_EMAIL',
+	'FIREBASE_PRIVATE_KEY',
+	'JWT_SECRET'
+	]
+const missing = required.filter(variable=> !process.env[variabe])
+if(missing.length) throw new Error(`Missing env vars: $ {missing.join(', ')}`)
